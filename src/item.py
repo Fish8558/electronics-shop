e2e8a69700f0c556406c3ad0f7c45b4d603ee203
Stas <stas_sever@mail.ru>
2023-12-27 00:23:19 +0500

    HW1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+from src.error import InstantiateCSVError
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __add_(self, other):
+        if isinstance(other, self.__class__):
+            return self.quantity + other.quantity
+        return None
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +38,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = int(self.price * self.pay_rate)
+
