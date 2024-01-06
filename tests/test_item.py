279806a2e9aeaf3a2137415ea79ec8a2e766a8fd
Stas <stas_sever@mail.ru>
2024-01-06 23:39:53 +0500

    test add

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,15 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 import pytest
-
+from config import CSV_PATH
 
 @pytest.fixture
 def test_item():
     return Item('fff', 100.0, 2)
-
-
-def test_item_add(test_item, test_phone):
-    assert test_item + test_phone == 12
 
 
 def test_item_repr(test_item):
@@ -37,6 +33,13 @@ def test_calculate_total_price(test_item):
     assert test_item.calculate_total_price() == 200.0
 
 
+def test_name(test_item):
+    test_item.name = 'iphone'
+    assert test_item.name == 'iphone'
+    test_item.name = 'iphoneXXXL1500'
+    assert test_item.name == 'iphoneXXXL'
+
+
 def test_string_to_number(test_item):
     assert type(test_item.string_to_number('3')) == int
     assert type(test_item.string_to_number('3.0')) == int
@@ -44,5 +47,5 @@ def test_string_to_number(test_item):
 
 
 def test_instantiate_from_csv(test_item):
-    test_item.instantiate_from_csv()
+    test_item.instantiate_from_csv(CSV_PATH)
     assert len(test_item.all) == 5
